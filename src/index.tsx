7b06e2979adb1997bb4c50bc7d5118b2d2379cef
RenanGeorgio <regeo9@hotmail.com>
2024-08-23 04:34:08 -0300

    feat
    mudança para vite

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import "@styles/global.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
