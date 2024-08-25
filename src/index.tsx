e265a828b7ed3a56fcb0717338dacc63a8f7efaa
RenanGeorgio <regeo9@hotmail.com>
2024-08-25 20:09:27 -0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "@styles/global.css";
+//import "@styles/global.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
