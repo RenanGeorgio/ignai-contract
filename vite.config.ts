c76f7846c6a1addd2f722afc16ac323017ab3668
RenanGeorgio <regeo9@hotmail.com>
2024-08-25 20:47:07 -0300

    try

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,24 @@ function get_paths_from_tsconfig() {
 
 export default defineConfig({
   resolve: {
-    alias: get_paths_from_tsconfig(),
+    alias: {
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@libs": path.resolve(__dirname, "./src/libs"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+      "@providers": path.resolve(__dirname, "./src/contexts/providers"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@controllers": path.resolve(__dirname, "./src/controllers"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@helpers": path.resolve(__dirname, "./src/helpers"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
   build: {
     target: 'esnext'
