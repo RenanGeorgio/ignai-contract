718b61d08b356c51e020049e654bac160e41f222
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-09-13 14:37:03 -0300

    fix: ajustado formulário
    Ajustado formulário da página de Buscador.

--- src/components/form/FormBuscador.tsx
+++ src/components/form/FormBuscador.tsx
@@ -1,120 +1,93 @@
-import { Box, Button, Checkbox, Flex, Input, Select, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Select, Text } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 import "@styles/viewsForm.css";
 
 const FormularioBuscaDetalhada: FunctionComponent = () => {
   return (
     <Box className="containerViewForm" p={5} border="1px solid" borderColor="gray.300" borderRadius="md" height="75%" overflowY="auto">
-      <Flex mb={4}>
-        <Box width="30%">
+      <Flex mb={4} align="center" wrap="wrap">
+        <Box className="flex-item">
           <Text mb={2}>Unidade Administrativa</Text>
-          <Select>
+          <Select className="form-field">
             <option>Todas</option>
           </Select>
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Número do Contrato</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Número do Processo</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
       </Flex>
 
-      <Flex mb={4}>
-        <Box width="30%">
+      <Flex mb={4} align="center" wrap="wrap">
+        <Box className="flex-item">
           <Text mb={2}>Enquadramento</Text>
-          <Select>
+          <Select className="form-field">
             <option>Todas</option>
           </Select>
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Situação</Text>
-          <Select>
+          <Select className="form-field">
             <option>Todas</option>
           </Select>
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Objeto da Contratação</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
       </Flex>
 
-      <Flex mb={4}>
-        <Box width="30%">
+      <Flex mb={4} align="center" wrap="wrap">
+        <Box className="flex-item">
           <Text mb={2}>Fornecedor</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>CNPJ/CPF</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Objeto de Contratação</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
       </Flex>
 
-      <Flex mb={4} align="center" justify="space-between">
-        <Box width="23%">
+      <Flex mb={4} align="center" justify="space-between" wrap="wrap">
+        <Box className="flex-item">
           <Text mb={2}>Início da Vigência</Text>
           <Text mb={2}>De:</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="23%">
+        <Box className="flex-item">
           <Text mb={2}>&nbsp;</Text>
           <Text mb={2}>Para:</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="23%">
+        <Box className="flex-item">
           <Text mb={2}>Fim da Vigência</Text>
           <Text mb={2}>De:</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="23%">
+        <Box className="flex-item">
           <Text mb={2}>&nbsp;</Text>
           <Text mb={2}>Para:</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
       </Flex>
 
       <Text mb={2}>Valor do Contrato</Text>
-      <Flex mb={4}>
-        <Box width="30%">
+      <Flex mb={4} align="center">
+        <Box className="flex-item">
           <Text mb={2}>De:</Text>
-          <Input />
+          <Input className="form-field" />
         </Box>
-        <Box width="30%" ml={2}>
+        <Box className="flex-item" ml={2}>
           <Text mb={2}>Para:</Text>
-          <Input />
-        </Box>
-      </Flex>
-
-      <Text mb={2}>Busca Detalhada</Text>
-      <Flex justifyContent="space-between">
-        <Box>
-          <Checkbox>Todos</Checkbox><br />
-          <Checkbox>Órgão Superior</Checkbox><br />
-          <Checkbox>Órgão Subordinado</Checkbox><br />
-          <Checkbox>Unidade Administrativa</Checkbox><br />
-          <Checkbox>Número do Processo</Checkbox>
-        </Box>
-        <Box>
-          <Checkbox>Número do ICJ</Checkbox><br />
-          <Checkbox>Enquadramento do Processo</Checkbox><br />
-          <Checkbox>CNPJ/CPF</Checkbox>
-        </Box>
-        <Box>
-          <Checkbox>Objeto da contratação</Checkbox><br />
-          <Checkbox>Valor do Contrato</Checkbox><br />
-          <Checkbox>Saldo bruto do contrato</Checkbox><br />
-          <Checkbox>Saldo inativo</Checkbox>
-        </Box>
-        <Box>
-          <Checkbox>Situação</Checkbox><br />
-          <Checkbox>Número dos aditivos</Checkbox><br />
-          <Checkbox>Fundamento Legal</Checkbox>
+          <Input className="form-field" />
         </Box>
       </Flex>
 
